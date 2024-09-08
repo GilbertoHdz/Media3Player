88a6fbbee8bde17b813cc4d1029dd7db99de4d85
Gilberto Hernandez G <Gilberto.Hernandez.-ND@disney.com>
2024-09-07 19:58:32 -0600

    task: creating video and music ui components

--- video/src/main/java/com/gilbertohdz/media3/video/ui/player/PlayerScreenState.kt
+++ video/src/main/java/com/gilbertohdz/media3/video/ui/player/PlayerScreenState.kt
@@ -1,6 +1,7 @@
 package com.gilbertohdz.media3.video.ui.player
 
 import com.gilbertohdz.media3.video.model.Video
+import com.gilbertohdz.media3.video.ui.components.ControlsListener
 
 data class PlayerScreenState(
     val isPlaying: Boolean = false,
@@ -9,5 +10,6 @@ data class PlayerScreenState(
     val bufferedPosition: Long = 0L,
     val currentVideo: Video? = null,
     val playlist: List<Video> = emptyList(),
+    val controlsListener: ControlsListener = object : ControlsListener {},
     val error: Exception? = null,
 )
