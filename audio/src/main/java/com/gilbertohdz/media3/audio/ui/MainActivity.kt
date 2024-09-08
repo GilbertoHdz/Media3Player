f7e11ac16d3636d1cfa5d69fd673d9d53ab6deb5
Gilberto Hernandez G <Gilberto.Hernandez.-ND@disney.com>
2024-09-07 20:46:37 -0600

    task: MediaSession setup on Activity

--- audio/src/main/java/com/gilbertohdz/media3/audio/ui/MainActivity.kt
+++ audio/src/main/java/com/gilbertohdz/media3/audio/ui/MainActivity.kt
@@ -5,9 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +13,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
 import com.gilbertohdz.media3.audio.ui.player.PlayerScreen
 import com.gilbertohdz.media3.audio.ui.player.PlayerScreenViewModel
 import com.gilbertohdz.media3.audio.ui.theme.Media3PlayerTheme
@@ -25,6 +26,9 @@ class MainActivity : ComponentActivity() {
     private val viewmodel by viewModels<PlayerScreenViewModel> {
         ViewModelProvider.AndroidViewModelFactory.getInstance(application)
     }
+
+    lateinit var mediaSession: MediaSession
+        private set
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,14 +41,34 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        val audioAttrs = AudioAttributes.Builder()
+            .setUsage(C.USAGE_MEDIA)
+            .setContentType(C.AUDIO_CONTENT_TYPE_MUSIC) // Router the audio with headphones or speaker
+            .build()
+
+        val player = ExoPlayer.Builder(this)
+            .setAudioAttributes(audioAttrs, true) // Auto pause when other app start playing
+            .setHandleAudioBecomingNoisy(true) // when unplug headphone will pause the player
+            .build()
+
+        mediaSession = MediaSession.Builder(this, player)
+            .build()
     }
 
     override fun onStart() {
         super.onStart()
+        viewmodel.onStart()
     }
 
     override fun onStop() {
         super.onStop()
+    }
+
+    override fun onDestroy() {
+        mediaSession.player.release()
+        mediaSession.release()
+        super.onDestroy()
     }
 }
 
