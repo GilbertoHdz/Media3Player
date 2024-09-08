3947edea15a57d06445affb530a1600957bc154d
Gilberto Hernandez G <Gilberto.Hernandez.-ND@disney.com>
2024-09-08 04:06:17 -0600

    task: Setup playerScreen and implement components for player and list

--- video/src/main/java/com/gilbertohdz/media3/video/ui/MainActivity.kt
+++ video/src/main/java/com/gilbertohdz/media3/video/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,22 +12,56 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.media3.common.AudioAttributes
+import androidx.media3.common.C
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
+import com.gilbertohdz.media3.video.ui.player.PlayerScreen
 import com.gilbertohdz.media3.video.ui.theme.Media3PlayerTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var mediaSession: MediaSession
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setupMediaSession()
         setContent {
             Media3PlayerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Media3 Video",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                PlayerScreen(mediaSession.player, mediaSession.token)
             }
         }
+    }
+
+    override fun onDestroy() {
+        mediaSession.player.release()
+        mediaSession.release()
+        super.onDestroy()
+    }
+
+    @OptIn(UnstableApi::class)
+    private fun setupMediaSession() {
+        val audioAttrs = AudioAttributes.Builder()
+            .setUsage(C.USAGE_MEDIA)
+            .setContentType(C.AUDIO_CONTENT_TYPE_MOVIE)
+            .build()
+
+        val player = ExoPlayer.Builder(this)
+            .setAudioAttributes(audioAttrs, true)
+            .setHandleAudioBecomingNoisy(true)
+            .setVideoScalingMode(C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING)
+            .build()
+
+        player.addListener(object : Player.Listener {
+            override fun onIsPlayingChanged(isPlaying: Boolean) {
+                // TODO: Add/Remove window FLAG_KEEP_SCREEN_ON
+            }
+        })
+
+        mediaSession = MediaSession.Builder(this, player).build()
     }
 }
 
